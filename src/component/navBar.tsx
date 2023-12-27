7e6b1c35a27f7ef1e51d1e511728f062921b41ea
rizarudesu <rizal.achmad@visionet.co.id>
2023-12-27 16:20:34 +0700

    init component navbar
    create Component Navbar

---
+++ src/component/navBar.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { useRouter } from "next/navigation";
+
+function NavBar({ items, opens }: { items: any; opens: () => void }) {
+  const router = useRouter();
+  return (
+    <div className="p-4 xl:ml-80">
+      <nav className="block w-full max-w-full bg-transparent text-white shadow-none rounded-xl transition-all px-0 py-1">
+        <div className="flex flex-col-reverse justify-between gap-6 md:flex-row md:items-center">
+          <div className="capitalize">
+            <nav aria-label="breadcrumb" className="w-max">
+              <ol className="flex flex-wrap items-center w-full bg-opacity-60 rounded-md bg-transparent p-0 transition-all">
+                <li className="flex items-center text-blue-gray-900 antialiased font-sans text-sm font-normal leading-normal cursor-pointer transition-colors duration-300 hover:text-light-blue-500">
+                  <button
+                    onClick={() => {
+                      router.replace(items.link);
+                    }}
+                  >
+                    <p className="block antialiased font-sans text-sm leading-normal text-blue-900 font-normal opacity-50 transition-all hover:text-blue-500 hover:opacity-100">
+                      {items.label}
+                    </p>
+                  </button>
+                  <span className="text-gray-500 text-sm antialiased font-sans font-normal leading-normal mx-2 pointer-events-none select-none">
+                    /
+                  </span>
+                </li>
+                <li className="flex items-center text-blue-900 antialiased font-sans text-sm font-normal leading-normal cursor-pointer transition-colors duration-300 hover:text-blue-500">
+                  <p className="block antialiased font-sans text-sm leading-normal text-blue-gray-900 font-normal">
+                    {items.link}
+                  </p>
+                </li>
+              </ol>
+            </nav>
+            <h6 className="block antialiased tracking-normal font-sans text-base font-semibold leading-relaxed text-gray-900">
+              {items.label}
+            </h6>
+          </div>
+          <div className="flex items-center">
+            <button
+              className="relative middle none font-sans font-medium text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none w-10 max-w-[40px] h-10 max-h-[40px] rounded-lg text-xs text-gray-500 hover:bg-blue-gray-500/10 active:bg-blue-gray-500/30 grid xl:hidden"
+              type="button"
+              onClick={opens}
+            >
+              <span className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  aria-hidden="true"
+                  stroke-width="3"
+                  className="h-6 w-6 text-blue-gray-500"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </span>
+            </button>
+            <a href="#">
+              <span className="flex flex-row middle none font-sans font-bold center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 rounded-lg text-gray-500 hover:bg-blue-gray-500/10 active:bg-blue-gray-500/30  items-center gap-1 px-4 xl:flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  aria-hidden="true"
+                  className="h-5 w-5 text-blue-gray-500"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+                Developer [ADMIN]
+              </span>
+            </a>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default NavBar;

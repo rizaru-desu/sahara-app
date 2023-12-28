39344284994fc13772b111538233e2f481770230
rizarudesu <rizal.achmad@visionet.co.id>
2023-12-29 00:56:45 +0700

    add bg
    add bg white

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} dark:bg-white bg-white`}>
+        {children}
+      </body>
     </html>
   );
 }

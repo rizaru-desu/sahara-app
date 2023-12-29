5b5fd6ba8b97d49b49727142113e1a2f6c1194bd
rizarudesu <rizal.achmad@visionet.co.id>
2023-12-30 04:21:53 +0700

    add cors

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://api.example.com/:path*",
+      },
+    ];
+  },
+};

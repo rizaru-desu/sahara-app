859a75726de5903ef629764eca5a0a687ffdca97
rizarudesu <rizal.achmad@visionet.co.id>
2023-12-30 04:07:08 +0700

    add button logout

--- src/app/component/navBar.tsx
+++ src/app/component/navBar.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { useRouter } from "next/navigation";
+import { FaPowerOff, FaUserCircle } from "react-icons/fa";
+import { BsFillMenuButtonWideFill } from "react-icons/bs";
 
 function NavBar({ items, opens }: { items: any; opens: () => void }) {
   const router = useRouter();
+
   return (
     <div className="p-4 xl:ml-80">
       <nav className="block w-full max-w-full bg-transparent text-white shadow-none rounded-xl transition-all px-0 py-1">
@@ -42,40 +45,22 @@ function NavBar({ items, opens }: { items: any; opens: () => void }) {
               onClick={opens}
             >
               <span className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  aria-hidden="true"
-                  strokeWidth="3"
-                  className="h-6 w-6 text-blue-gray-500"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
+                <BsFillMenuButtonWideFill size={15} color={"#000"} />
               </span>
             </button>
-            <a href="#">
+            <span>
               <span className="flex flex-row middle none font-sans font-bold center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 rounded-lg text-gray-500 hover:bg-blue-gray-500/10 active:bg-blue-gray-500/30  items-center gap-1 px-4 xl:flex">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  aria-hidden="true"
-                  className="h-5 w-5 text-blue-gray-500"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
+                <FaUserCircle size={15} color={"#000"} />
                 Developer [ADMIN]
               </span>
-            </a>
+            </span>
+
+            <button>
+              <span className="flex flex-row middle none font-sans font-bold center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 rounded-lg text-gray-500 hover:bg-blue-gray-500/10 active:bg-blue-gray-500/30  items-center gap-1 px-4 xl:flex">
+                <FaPowerOff size={15} color={"#000"} />
+                Log Out
+              </span>
+            </button>
           </div>
         </div>
       </nav>

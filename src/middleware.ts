ec9407b7c7718e8584e5af9872cf75e4edf2e7ed
rizarudesu <rizal.achmad@visionet.co.id>
2023-12-30 04:01:50 +0700

    add middleware
    to protected route

---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export const protectedRoutes = ["/dashboard"];
+export const authRoutes = ["/"]; //login page
+
+export async function middleware(request: NextRequest) {
+  const userData = request.cookies.get("userData");
+
+  if (authRoutes.includes(request.nextUrl.pathname) && userData) {
+    console.log("Redirecting to /dashboard");
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  if (protectedRoutes.includes(request.nextUrl.pathname) && !userData) {
+    console.log("Redirecting to /login");
+    request.cookies.delete("userData");
+    const response = NextResponse.redirect(new URL("/", request.url));
+    response.cookies.delete("userData");
+
+    return response;
+  }
+}

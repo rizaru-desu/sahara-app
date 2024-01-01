fb81c718c4f65b1873c1902e8d9e8c82cf94f178
rizarudesu <rizal.achmad@visionet.co.id>
2024-01-02 00:02:06 +0700

    fix svg

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -193,7 +193,7 @@ export default function Product() {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
                   className="h-4 w-4 text-red-700 dark:text-red-700"
                 >

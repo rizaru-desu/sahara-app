7b73be2871c86a3f598c190146e74f0ac4bb2f6c
rizarudesu <rizal.achmad@visionet.co.id>
2024-01-06 20:33:26 +0700

    add query db
    1. find many user filter
    2. get all user (pagination)
    3. find user (select key & value string map)
    4. update roles
    5. batch create user

--- src/app/utils/db/userDB.tsx
+++ src/app/utils/db/userDB.tsx
@@ -1,46 +1,145 @@
 import { PrismaClient } from "@prisma/client";
+import _ from "lodash";
 const prisma = new PrismaClient();
 
-//region find user by email
 export const findByEmail = async ({ email }: { email: string }) => {
   try {
     const user = await prisma.user.findUnique({ where: { email } });
     return user;
   } catch (error: any) {
-    // Instead of using "any" type, you can let TypeScript infer the type of the error
     throw new Error(error.message);
   } finally {
-    // Close the Prisma client to avoid leaks
     await prisma.$disconnect();
   }
 };
-//endregion
 
 export const findUser = async ({ userId }: { userId: string }) => {
   try {
     const user = await prisma.user.findUnique({
-      where: { userId },
+      where: {
+        userId,
+      },
       include: {
-        StringMap: true,
+        StringMap: {
+          select: {
+            key: true,
+            value: true,
+          },
+        },
       },
     } as any);
 
     return user;
   } catch (error: any) {
-    // Instead of using "any" type, you can let TypeScript infer the type of the error
     throw new Error(error.message);
   } finally {
-    // Close the Prisma client to avoid leaks
     await prisma.$disconnect();
   }
 };
 
-//region create user
+export const findManyUserFilter = async ({ value }: { value: string }) => {
+  try {
+    const result = await prisma.user.findMany({
+      where: {
+        OR: [
+          {
+            email: { contains: value },
+          },
+          { fullname: { contains: value } },
+          { phone: { contains: value } },
+        ],
+        NOT: { userId: "f9442d7a-5715-4fbf-ba77-947208a8c03e" },
+      },
+      select: {
+        userId: true,
+        fullname: true,
+        phone: true,
+        dateOfBirth: true,
+        email: true,
+        roleId: true,
+        verification: true,
+        createBy: true,
+        modifiedBy: true,
+        createdAt: true,
+        modifedAt: true,
+      },
+      orderBy: { fullname: "asc" },
+    });
+
+    return { result, totalCount: _.size(result) };
+  } catch (error: any) {
+    throw new Error(error.message);
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+export const manyUserPagination = async ({
+  skip,
+  take,
+}: {
+  skip: number;
+  take: number;
+}) => {
+  try {
+    const user = await prisma.user.findMany({
+      skip,
+      take,
+      select: {
+        userId: true,
+        fullname: true,
+        phone: true,
+        dateOfBirth: true,
+        email: true,
+        roleId: true,
+        verification: true,
+        createBy: true,
+        modifiedBy: true,
+        createdAt: true,
+        modifedAt: true,
+      },
+      where: {
+        NOT: { userId: "f9442d7a-5715-4fbf-ba77-947208a8c03e" },
+      },
+      orderBy: { fullname: "asc" },
+    });
+    const totalCount = await prisma.user.count();
+
+    return { user, totalCount };
+  } catch (error: any) {
+    throw new Error(error.message);
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+export const updateRoleUser = async ({
+  userId,
+  roleId,
+  fullname,
+}: {
+  userId: string;
+  roleId: string;
+  fullname: string;
+}) => {
+  try {
+    const user = await prisma.user.update({
+      where: { userId },
+      data: { roleId, modifiedBy: fullname },
+    });
+
+    return user;
+  } catch (error: any) {
+    throw new Error(error.message);
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
 export const createUser = async ({
   dataUser,
 }: {
   dataUser: {
-    userId: string;
     email: string;
     password: string;
     phone: string;
@@ -58,8 +157,20 @@ export const createUser = async ({
   } catch (error: any) {
     throw new Error(error.message);
   } finally {
-    // Close the Prisma client to avoid leaks
     await prisma.$disconnect();
   }
 };
-//endregion
+
+export const batchCreateUser = async ({ dataUser }: { dataUser: [] }) => {
+  try {
+    const result = await prisma.user.createMany({
+      data: dataUser,
+    });
+
+    return result;
+  } catch (error: any) {
+    throw new Error(error.message);
+  } finally {
+    await prisma.$disconnect();
+  }
+};

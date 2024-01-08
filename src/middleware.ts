e49a61ac225ebaed61be9c4ee7b0e423752e5381
rizarudesu <rizal.achmad@visionet.co.id>
2024-01-09 00:45:37 +0700

    add path

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,29 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export const protectedRoutes = ["/dashboard", "/product/", "/product"];
-export const authRoutes = ["/"]; //login page
+export const protectedRoutes = [
+  "/dashboard",
+  "/product/",
+  "/product",
+  "/stock",
+  "/setting",
+  "/customer",
+  "/customer-report",
+  "/delivery-order",
+  "/list-user",
+  "/pointby-report",
+  "/labeling",
+];
+export const authRoutes = ["/"];
 
 export async function middleware(request: NextRequest) {
   const userData = request.cookies.get("userData");
 
   if (authRoutes.includes(request.nextUrl.pathname) && userData) {
-    console.log("Redirecting to /dashboard");
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
   if (protectedRoutes.includes(request.nextUrl.pathname) && !userData) {
-    console.log("Redirecting to /login");
     request.cookies.delete("userData");
     const response = NextResponse.redirect(new URL("/", request.url));
     response.cookies.delete("userData");

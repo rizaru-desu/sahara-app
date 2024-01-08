f60dc76d7ebb93e0a74b0fb268e686fd6913bab6
rizarudesu <rizal.achmad@visionet.co.id>
2024-01-09 00:45:47 +0700

    add mail cofig

---
+++ src/app/utils/services/node.mailer.tsx
@@ -0,0 +1,38 @@
+"use strict";
+
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: 587,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASSWORD,
+  },
+  tls: { rejectUnauthorized: false },
+});
+
+// async..await is not allowed in the global scope, must use a wrapper
+export default async function sendMailer({
+  send,
+  subject,
+  html,
+}: {
+  send: string;
+  subject: string;
+  html: any;
+}) {
+  try {
+    const info = await transporter.sendMail({
+      from: '"no-reply" <no-reply@meou.my.id>',
+      to: send,
+      subject: subject,
+      html: html, // use the html parameter passed to the function
+    });
+
+    return info;
+  } catch (error: any) {
+    console.log(error);
+    throw new Error(error.message);
+  }
+}

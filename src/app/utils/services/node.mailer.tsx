9808296568b551344cd03dc706d6b4993b47b2cb
rizarudesu <rizal.achmad@visionet.co.id>
2024-02-27 15:49:19 +0700

    fix port

--- src/app/utils/services/node.mailer.tsx
+++ src/app/utils/services/node.mailer.tsx
@@ -4,7 +4,7 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
-  port: 587,
+  port: Number(process.env.MAIL_PORT),
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASSWORD,

7827de81691f7051ff5939ff03db5a5821312c93
rizarudesu <rizal.achmad@visionet.co.id>
2024-02-29 17:14:25 +0700

    add DR

---
+++ src/app/api/controller/mobile/addDR/route.tsx
@@ -0,0 +1,83 @@
+import { type NextRequest, NextResponse } from "next/server";
+import { validateToken } from "@/app/utils/token/validate";
+import { findStockBox } from "@/app/utils/db/controllerDB";
+import z from "zod";
+import _ from "lodash";
+
+const Schema = z
+  .object({
+    data: z.unknown(),
+  })
+  .strict();
+
+function validateSchema({ data }: { data: any }) {
+  try {
+    const parseData = Schema.parse(data);
+    return parseData;
+  } catch (error: any) {
+    if (error.issues && error.issues.length > 0) {
+      const validationErrors = error.issues.map((issue: any) => ({
+        path: issue.path.join("."),
+        message: issue.message,
+      }));
+
+      const errorMessage = validationErrors
+        .map((error: any) => `Field '${error.path}' ${error.message}`)
+        .join(" \n");
+
+      throw new Error(errorMessage);
+    } else {
+      throw new Error("Invalid Schema.");
+    }
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const token = request.headers.get("Authorization") as any;
+
+    const tokenWithoutBearer = token?.replace(/^Bearer\s+/i, "") || undefined;
+    const userData = request.cookies.get("userData");
+
+    const tokenValidated = (await validateToken({
+      token: _.isEmpty(tokenWithoutBearer)
+        ? userData?.value
+        : tokenWithoutBearer,
+    })) as any;
+
+    const json = await request.json();
+
+    const resultValid = validateSchema({
+      data: json,
+    });
+
+    if (tokenValidated) {
+      return NextResponse.json(
+        {
+          message: resultValid,
+        },
+        {
+          status: 200,
+        }
+      );
+    } else {
+      return NextResponse.json(
+        {
+          message: "Invalid token. Authentication failed.",
+        },
+        {
+          status: 401,
+        }
+      );
+    }
+  } catch (error: any) {
+    return NextResponse.json(
+      {
+        message: error.message,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
